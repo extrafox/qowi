0e2505fe66db44322d671cd34082c1d8a5fdd0f1
Christopher Taylor <christopher@extrafox.me>
2025-01-13 23:14:03 -0800

    forward and reverse lookup tables working. tested with 4-bits.

--- scripts/test_haar_sorting.py
+++ scripts/test_haar_sorting.py
@@ -217,7 +217,6 @@ class TestHaarSortTable(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.table.haar_sort_index_to_grid(9999999, self.forward_table)
 
-
 if __name__ == "__main__":
     unittest.main()
 
